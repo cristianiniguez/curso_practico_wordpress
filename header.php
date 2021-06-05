d01dc21cafbf06fced64ede522cc7fb32868a68d
cristianiniguez <felizpc1@gmail.com>
2021-06-05 18:23:36 -0400

    construir vistas: generando el menu

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
@@ -12,9 +12,24 @@
 
   <header>
     <div class="container">
-      <div class="row">
+      <div class="row align-items-center">
         <div class="col-4">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo de PlatziGifts">
+        </div>
+        <div class="col-8">
+          <nav>
+            <?php
+
+            wp_nav_menu(
+              array(
+                'theme_location' => 'top_menu',
+                'menu_class' => 'menu-principal',
+                'container_class' => 'container-menu'
+              )
+            );
+
+            ?>
+          </nav>
         </div>
       </div>
     </div>
